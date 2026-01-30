83e44450f87892fb4569f80f0ee32ea80ce9f12f
BismuthTime <122326994+echo2334@users.noreply.github.com>
2026-01-29 17:04:12 -0700

    Created shooter subsystem

---
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Subsystems/Shooter.java
@@ -0,0 +1,57 @@
+package frc.robot.Subsystems;
+
+import frc.robot.Interfaces.Subsystem;
+
+public class Shooter implements Subsystem {
+    private static Shooter instance = null;
+
+    public static Shooter getInstance() {
+        if (instance == null) {
+            instance = new Shooter();
+        }
+        return instance;
+    }
+
+    public Shooter(){
+        //MOTORs 
+        //other devices
+
+    }
+    public double getSpeed(){
+    return 0;
+    //to do
+     }
+    
+     public void stop(){
+
+    }
+    public void shoot(){
+
+    }
+    String state="stop";
+
+
+    public void update(){
+        //run in loop    
+        switch (state){
+            case "shooting":
+            break; 
+            
+        }
+    }
+
+    public void initialize(){
+    }
+
+    public void log(){
+
+    }
+
+    public boolean isEnabled(){
+        return true;
+    }
+
+    public String getName(){
+        return "Shooter";
+    }
+}
