c0c70c429373f95740a4128a1eb526a1f4ed669a
FRC8334Students <rhea.sneller@gmail.com>
2026-02-07 10:21:56 -0700

    Trying inverted things, changed it back for ethical reasons.

--- TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
@@ -46,7 +46,7 @@ public class Teleop {
         double strafe; //Rhea this means going side to side
         double rotation = 0;
 
-        boolean isFieldOrriented = false;
+        boolean isFieldOrriented = true;
 
         if (Math.abs(controllerLeftY) >= 0.1) {
             forward = -(controllerLeftY) * Constants.MAX_SPEED;
@@ -81,7 +81,7 @@ public class Teleop {
         if(isFieldOrriented) //translation2d is used for lateral movement of the swerve drive
         {
             
-            swerveBase.driveFieldOriented(swerveBase.getTargetSpeeds(forward, strafe, new Rotation2d(-rotationX, -rotationY)));
+            //swerveBase.driveFieldOriented(swerveBase.getTargetSpeeds(forward, strafe, new Rotation2d(-rotationX, -rotationY)));
             swerveBase.drive(new Translation2d(forward,strafe), rotation, true);
         }
         else 
