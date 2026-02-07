f3ac7badef87d3f126b6d3ec6041a65c6147132a
thetruerook <austincromar11@gmail.com>
2026-02-07 13:43:50 -0700

    adding joystick and xbox controller switch

--- TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
@@ -3,6 +3,7 @@ package frc.robot;
 import frc.robot.Devices.Controller;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -14,7 +15,9 @@ public class Teleop {
 
     Controller driverController; //object of Controller
     SwerveBase swerveBase; //object of SwerveBase
+    Joystick joystickController; //object of joystick
 
+    public static boolean JoystickEnabled = false;
     private double controllerLeftX; //variable for the left x joystick axis
     private double controllerLeftY; //variable for the left y joystick axis
     private double controllerRightX; //variable for the right x joystick axis
@@ -25,28 +28,49 @@ public class Teleop {
     double rotationY;
 
     public Teleop() {
-        driverController = new Controller(PortMap.DRIVER_CONTROLLER); //creates a new controller
         swerveBase = SwerveBase.getInstance(); //gets an instance of SwerveBase
+        if (JoystickEnabled == false){
+            driverController = new Controller(PortMap.DRIVER_CONTROLLER);
+        }
+        else{
+            joystickController = new Joystick(PortMap.DRIVER_CONTROLLER);
+        }
     }
+
+
 
     public void teleopPeriodic() //everything in this method will get executed 
     {
         driveBaseControl(); //executes the driveBaseControl method
     }
     
+    
+
+
     public void driveBaseControl() {
-        controllerLeftY = driverController.getLeftY(); //sets the variable controllerLeftY to the actual data coming from the controller
-        controllerLeftX = driverController.getLeftX(); //sets the variable controllerLeftX to the actual data coming from the controller
-        controllerRightX = driverController.getRightX(); //sets the variable controllerRightX to the actual data coming from the controller
-        controllerRightY = driverController.getRightY(); //sets the variable controllerRightY to the actual data coming from the controller
-        controllerAButton = driverController.getAButton();
-        controllerRightBumper = driverController.getRightBumperButton(); //sets the variable controllerRightBumper to the actual data coming from the controller
+        boolean isFieldOrriented = true;
+
+        if (JoystickEnabled == false){
+            controllerLeftY = driverController.getLeftY(); //sets the variable controllerLeftY to the actual data coming from the controller
+            controllerLeftX = driverController.getLeftX(); //sets the variable controllerLeftX to the actual data coming from the controller
+            controllerRightX = driverController.getRightX(); //sets the variable controllerRightX to the actual data coming from the controller
+            controllerRightY = driverController.getRightY(); //sets the variable controllerRightY to the actual data coming from the controller
+            controllerAButton = driverController.getAButton();
+            controllerRightBumper = driverController.getRightBumperButton(); //sets the variable controllerRightBumper to the actual data coming from the controller
+        }
+        else{
+            controllerLeftY = joystickController.getY(); //sets the variable controllerLeftY to the actual data coming from the controller
+            controllerLeftX = joystickController.getX(); //sets the variable controllerLeftX to the actual data coming from the controller
+            controllerRightX = joystickController.getTwist(); //sets the variable controllerRightX to the actual data coming from the controller
+            controllerRightY = 0; //sets the variable controllerRightY to the actual data coming from the controller
+            controllerAButton = driverController.getAButton();
+            controllerRightBumper = driverController.getRightBumperButton(); //sets the variable controllerRightBumper to the actual data coming from the controller
+        }
 
         double forward; 
         double strafe; //Rhea this means going side to side
         double rotation = 0;
 
-        boolean isFieldOrriented = true;
 
         if (Math.abs(controllerLeftY) >= 0.1) {
             forward = -(controllerLeftY) * Constants.MAX_SPEED;
