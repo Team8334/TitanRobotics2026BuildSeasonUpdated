c529c5d86fba7cd4b7200ddd2145bb6c3f0d2fba
FRC8334Students <rhea.sneller@gmail.com>
2026-02-17 16:54:37 -0700

    just disabled field oriented, can be turned back on

--- TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
@@ -47,7 +47,7 @@ public class Teleop {
         double rotation = 0;
 
 
-        boolean isFieldOrriented = true;
+        boolean isFieldOrriented = false;
 
         if (Math.abs(controllerLeftY) >= 0.1) {
             forward = (controllerLeftY) * Constants.MAX_SPEED;
@@ -81,7 +81,7 @@ public class Teleop {
         if(isFieldOrriented) //translation2d is used for lateral movement of the swerve drive
         {
             //swerveBase.driveFieldOriented(swerveBase.getTargetSpeeds(forward, strafe, new Rotation2d(-rotationY, -rotationX)));
-            swerveBase.drive(new Translation2d(forward,strafe), rotation, true);
+            swerveBase.drive(new Translation2d(forward,strafe), rotation, false);
         }
         else 
         {
