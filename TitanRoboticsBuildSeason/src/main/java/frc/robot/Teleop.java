28f32c358e9fb93bd7bcb6e770c107c5d7b8ef41
thetruerook <157543836+thetruerook@users.noreply.github.com>
2026-02-24 15:25:05 -0700

    field oriented working

--- TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
@@ -134,7 +134,7 @@ public class Teleop {
         if(isFieldOrriented) //translation2d is used for lateral movement of the swerve drive
         {
             //swerveBase.driveFieldOriented(swerveBase.getTargetSpeeds(forward, strafe, new Rotation2d(-rotationY, -rotationX)));
-            swerveBase.drive(new Translation2d(forward,strafe), rotation, false);
+            swerveBase.drive(new Translation2d(forward,strafe), rotation, true);
         }
         else 
         {
