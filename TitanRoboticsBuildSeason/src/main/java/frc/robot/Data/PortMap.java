1b6d22b2a9f1ffba557f00b413664dba42348376
Sarah <sarnson@comcast.net>
2026-03-02 16:58:39 -0700

    Added Hopper

--- TitanRoboticsBuildSeason/src/main/java/frc/robot/Data/PortMap.java
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Data/PortMap.java
@@ -4,5 +4,6 @@ public final class PortMap {
 
     public static final int DRIVER_CONTROLLER = 0;
     public static int armMotor = 1;
+    public static final int HOPPER_MOTOR_CANID = 0;
     
 }
--- TitanRoboticsBuildSeason/src/main/java/frc/robot/Subsystems/intake/hopper.java
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Subsystems/intake/hopper.java
@@ -2,9 +2,49 @@ package frc.robot.Subsystems.intake;
 
 import frc.robot.Interfaces.Subsystem;
 import frc.robot.Subsystems.SubsystemManager;
+import frc.robot.Data.PortMap;
 import frc.robot.Devices.ModifiedEncoder;
 import frc.robot.Devices.NeoSparkMaxMotor;
 
-public class hopper {
-    public double voltage;
+public class Hopper implements Subsystem {
+    private static Hopper instance = null;
+
+     public static Hopper getInstance() {
+        if (instance == null) {
+            instance = new Hopper();
+        }
+        return instance;
+    }
+
+    public NeoSparkMaxMotor hopperMotor;
+
+
+    public Hopper() {
+    hopperMotor = new NeoSparkMaxMotor(PortMap.HOPPER_MOTOR_CANID);
+    }
+
+    public void setSpeed(double speed) {
+        hopperMotor.set(speed);
+    }
+
+    public double voltage; 
+
+
+    public void update() {
+    }
+
+    public void initialize() {
+    }
+
+    public void log() {
+
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getName() {
+        return "Hopper";
+    }
 }
