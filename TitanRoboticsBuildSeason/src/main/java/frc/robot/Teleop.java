58fb28904f938cc5089d72cc69526f7c55a7307c
thetruerook <157543836+thetruerook@users.noreply.github.com>
2026-03-03 18:32:02 -0700

    teleop formatting
    formatted teleop removed duplicates

--- TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Teleop.java
@@ -1,192 +1,193 @@
 package frc.robot;
 
-import frc.robot.Devices.Controller;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import frc.robot.Data.Constants;
+import frc.robot.Data.PortMap;
+import frc.robot.Devices.Controller;
 import frc.robot.Subsystems.Shooter;
 import frc.robot.Subsystems.Shooter.ShootingSolution;
 import frc.robot.Subsystems.SwerveBase;
 import frc.robot.Subsystems.intake.Hopper;
 import frc.robot.Subsystems.intake.IntakeMechanism;
-import frc.robot.Data.PortMap;
-import frc.robot.Data.Constants;
 
 public class Teleop {
 
-    Controller driverController; //object of Controller
-    Controller operatorController; // object of Controller for the operator
-    IntakeMechanism intakeMechanism; // object of Intake Mechanism
-    SwerveBase swerveBase; //object of SwerveBase
-    Joystick joystickController; //object of joystick
+    // Subsystems
+    SwerveBase swerveBase;
+    IntakeMechanism intakeMechanism;
     Shooter shooter;
-    ShootingSolution shootingSolution;
-    Controller opController;
     Hopper hopper;
 
-    public static boolean JoystickEnabled = false;
-    private double controllerLeftX; //variable for the left x joystick axis
-    private double controllerLeftY; //variable for the left y joystick axis
-    private double controllerRightX; //variable for the right x joystick axis
-    private double controllerRightTrigger; // axis
-    private double controllerLeftTrigger; //variable for if the left trigger is pressed
-    private double controllerRightY;
-    private boolean controllerAButton; 
-    private boolean controllerRightBumper; //variable for if the right bumper is pressed
-    private double opControllerLeftY;
+    // Controllers
+    Controller driverController;
+    Controller operatorController;
+    Joystick joystickController;
+
+    // Configuration
+    public static boolean joystickEnabled = false;
+
+    // Driver State
+    private double driverLeftX;
+    private double driverLeftY;
+    private double driverRightX;
+    private double driverRightY;
+    private boolean driverAButton;
+    private boolean driverRightBumper;
+
+    // Operator State
+    private double operatorLeftY;
+    private double operatorLeftTrigger;
+    private double operatorRightTrigger;
+    private boolean operatorAButton;
+    private boolean operatorRightBumper;
+
     double rotationX;
     double rotationY;
+    ShootingSolution shootingSolution;
 
     public Teleop() {
-        swerveBase = SwerveBase.getInstance(); //gets an instance of SwerveBase
-        operatorController = new Controller(PortMap.OPERATOR_CONTROLLER);
-        swerveBase = SwerveBase.getInstance(); // gets an instance of SwerveBase
+        swerveBase = SwerveBase.getInstance();
         shooter = Shooter.getInstance();
         intakeMechanism = IntakeMechanism.getInstance();
         hopper = Hopper.getInstance();
-        
-        if (JoystickEnabled == false){
+
+        operatorController = new Controller(PortMap.OPERATOR_CONTROLLER);
+
+        if (!joystickEnabled) {
             driverController = new Controller(PortMap.DRIVER_CONTROLLER);
-        }
-        else{
+        } else {
             joystickController = new Joystick(PortMap.DRIVER_CONTROLLER);
         }
     }
 
-    public void teleopPeriodic() //everything in this method will get executed 
-    {
-        driveBaseControl(); //executes the driveBaseControl method
-        IntakeControl(); 
+    public void teleopPeriodic() {
+        readControllers();
+        driveBaseControl();
+        intakeControl();
+        operatorControl();
     }
 
-    public void IntakeControl() {
-        if (driverController.getYButton()) {
-            intakeMechanism.setState("Standby");
+    private void readControllers() {
+        // Read Operator Controller
+        operatorLeftY = operatorController.getLeftY();
+        operatorLeftTrigger = operatorController.getLeftTriggerAxis();
+        operatorRightTrigger = operatorController.getRightTriggerAxis();
+        operatorAButton = operatorController.getAButton();
+        operatorRightBumper = operatorController.getRightBumper();
+
+        // Read Driver Controller
+        if (!joystickEnabled) {
+            driverLeftY = driverController.getLeftY();
+            driverLeftX = driverController.getLeftX();
+            driverRightX = driverController.getRightX();
+            driverRightY = 0;
+            driverAButton = driverController.getAButton();
+            driverRightBumper = driverController.getRightBumperButton();
+        } else {
+            driverLeftY = joystickController.getY();
+            driverLeftX = joystickController.getX();
+            // Assuming getTwist() mapped to driverRightX
+            driverRightX = joystickController.getTwist();
+            driverRightY = 0;
+            driverAButton = joystickController.getRawButton(1);
+            driverRightBumper = joystickController.getTop();
         }
-
-       if (driverController.getBButton()) {  
-        intakeMechanism.setState("Intaking");
-       }
-
-       if (driverController.getAButton()) {
-        intakeMechanism.setState("Reversed");
-       }
-
-       if (driverController.getXButton()) {
-        intakeMechanism.setState("Disabled");
-       }
     }
-    
+
+    public void intakeControl() {
+        // Intake uses driver buttons standardly per original code
+        if (driverController != null) {
+            if (driverController.getYButton()) {
+                intakeMechanism.setState("Standby");
+            }
+            if (driverController.getBButton()) {  
+                intakeMechanism.setState("Intaking");
+            }
+            if (driverController.getAButton()) {
+                intakeMechanism.setState("Reversed");
+            }
+            if (driverController.getXButton()) {
+                intakeMechanism.setState("Disabled");
+            }
+        }
+    }
+
     public void driveBaseControl() {
+        boolean isFieldOriented = true;
 
-        boolean isFieldOrriented = true;
-
-        if (JoystickEnabled == false){
-            controllerLeftY = driverController.getLeftY(); //sets the variable controllerLeftY to the actual data coming from the controller
-            controllerLeftX = driverController.getLeftX(); //sets the variable controllerLeftX to the actual data coming from the controller
-            controllerRightX = driverController.getRightX(); //sets the variable controllerRightX to the actual data coming from the controller
-            controllerRightY = 0; //sets the variable controllerRightY to the actual data coming from the controller
-            controllerAButton = driverController.getAButton();
-            controllerRightBumper = driverController.getRightBumperButton(); //sets the variable controllerRightBumper to the actual data coming from the controller
-        }
-        else{
-            controllerLeftY = joystickController.getY(); //sets the variable controllerLeftY to the actual data coming from the controller
-            controllerLeftX = joystickController.getX(); //sets the variable controllerLeftX to the actual data coming from the controller
-            controllerRightX = joystickController.getTwist(); //sets the variable controllerRightX to the actual data coming from the controller
-            controllerRightY = 0; //sets the variable controllerRightY to the actual data coming from the controller
-            controllerAButton = joystickController.getRawButton(1);
-            controllerRightBumper = joystickController.getTop(); //sets the variable controllerRightBumper to the actual data coming from the controller
-
-
-        }
-
-        double forward; 
-        double strafe; //Rhea this means going side to side
+        double forward;
+        double strafe;
         double rotation = 0;
 
-        if (Math.abs(controllerLeftY) >= 0.1) {
-            forward = (controllerLeftY) * Constants.MAX_SPEED;
+        if (Math.abs(driverLeftY) >= 0.1) {
+            forward = driverLeftY * Constants.MAX_SPEED;
         } else {
             forward = 0;
         }
-        if (Math.abs(controllerLeftX) >= 0.1) {
-            strafe = (controllerLeftX) * Constants.MAX_SPEED;
+
+        if (Math.abs(driverLeftX) >= 0.1) {
+            strafe = driverLeftX * Constants.MAX_SPEED;
         } else {
             strafe = 0;
         }
-        if (Math.abs(controllerRightX) >= 0.1) {
-            rotation = -((Math.abs(controllerRightX))*(controllerRightX)) * Constants.MAX_ROTATION_SPEED;
+
+        if (Math.abs(driverRightX) >= 0.1) {
+            rotation = -(Math.abs(driverRightX) * driverRightX) * Constants.MAX_ROTATION_SPEED;
         } else {
             rotation = 0;
         }
 
-       /*  if (Math.abs(controllerRightX) >= 0.5 || Math.abs(controllerRightY) >= 0.5)
-        {
-            rotationX = controllerRightX;
-            rotationY = controllerRightY;
-        } 
-        */
-        if (controllerAButton)
-        {
+        if (driverAButton) {
             swerveBase.zeroGyro();
             rotationX = 0;
             rotationY = -1;
         }
-        
-        if (Math.abs(opControllerLeftY) >=0.1) {
-            hopper.setSpeed(opControllerLeftY);
 
+        if (Math.abs(operatorLeftY) >= 0.1) {
+            hopper.setSpeed(operatorLeftY);
+        } else {
+            hopper.setSpeed(0);
         }
 
-        if(isFieldOrriented) //translation2d is used for lateral movement of the swerve drive
-        {
-            //swerveBase.driveFieldOriented(swerveBase.getTargetSpeeds(forward, strafe, new Rotation2d(-rotationY, -rotationX)));
-            swerveBase.drive(new Translation2d(forward,strafe), rotation, false);
-        }
-        else 
-        {
-            swerveBase.drive(new Translation2d(forward,strafe), rotation, false);
+        if (isFieldOriented) {
+            swerveBase.drive(new Translation2d(forward, strafe), rotation, true);
+        } else {
+            swerveBase.drive(new Translation2d(forward, strafe), rotation, false);
         }
     }
 
     public void operatorControl() {
-        controllerLeftTrigger = operatorController.getLeftTriggerAxis();
-        controllerRightTrigger = operatorController.getRightTriggerAxis();
-        controllerRightBumper = operatorController.getRightBumper();
-        double forward;
-        double strafe;
-
+        if (operatorRightTrigger >= 0.5) {
+            if (shootingSolution != null && shootingSolution.shotPossibilty()) {
+                shooter.shoot();
+            }
+        }
         
-        if (controllerRightTrigger >= 0.5) {
-            if (shootingSolution.shotPossibilty())
-            shooter.shoot();
+        if (operatorLeftTrigger > 0.05) {
+            shooter.manualSpeed(operatorLeftTrigger);
         }
-        if (controllerLeftTrigger > 0.05) {
-            shooter.manualSpeed(controllerLeftTrigger);
-        }
-        if (controllerAButton) {
+        
+        if (operatorAButton) {
             shooter.stop();
         }
 
         shootingSolution = shooter.calculateShootingSolution(swerveBase.getPose());
         
-        shooter.setTargetRPM(shootingSolution.flywheelRPM());
-        if (controllerRightBumper) {
-            if (shootingSolution.shotPossibilty()){
-                if (Math.abs(shootingSolution.shootingAngle().minus(swerveBase.getHeading()).getDegrees())<3){
-                    shooter.shoot();
+        if (shootingSolution != null) {
+            shooter.setTargetRPM(shootingSolution.flywheelRPM());
+            
+            if (operatorRightBumper) {
+                if (shootingSolution.shotPossibilty()) {
+                    if (Math.abs(shootingSolution.shootingAngle().minus(swerveBase.getHeading()).getDegrees()) <3) {
+                        shooter.shoot();
+                    } else {
+                        shooter.prepareToShoot();
+                    }
+                    swerveBase.driveFieldOriented(swerveBase.getTargetSpeeds(0, 0, shootingSolution.shootingAngle()));
                 }
-                else {
-                    shooter.prepareToShoot();
-                }
-                swerveBase.driveFieldOriented(swerveBase.getTargetSpeeds(0, 0, shootingSolution.shootingAngle()));
             }
         }
     }
