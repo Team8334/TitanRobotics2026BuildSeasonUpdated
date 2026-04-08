46873c145da0a299849415145b4e0d1a2a18c7f8
rsneller <rhea.sneller@gmail.com>
2026-04-07 18:20:17 -0600

    I added the intake action, cleaned up the auto trajectories, and added a mission that goes from the depot to intake and them shoots. The only problem is that we can't move while intaking because I did not learn how to bind commands to the choreo trajectories

---
+++ TitanRoboticsBuildSeason/src/main/java/frc/robot/Auto/Missions/DepotShootMission.java
@@ -0,0 +1,18 @@
+package frc.robot.Auto.Missions;
+
+import frc.robot.Auto.AutoMissionEndedException;
+import frc.robot.Auto.Actions.IntakeAction;
+import frc.robot.Auto.Actions.ShootAction;
+import frc.robot.Auto.Actions.MoveSwerve;
+import frc.robot.Auto.Actions.WaitAction;
+
+public class DepotShootMission extends MissionBase{
+    @Override
+    public void routine() throws AutoMissionEndedException{
+        runAction(new MoveSwerve("DepotPath", true));
+        runAction(new IntakeAction(7, "Intaking"));
+        runAction(new MoveSwerve("DepotToShootPath", true));
+        runAction(new ShootAction(10));
+    }
+
+}
